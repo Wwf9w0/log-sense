ba13aff1f63d10264bc8aada74123181d5d2b5ac
wwf9w0 <aliemrekaradag1@gmail.com>
2025-06-09 15:59:33 +0300

    first commit

---
+++ src/main/java/com/logsense/server/LogsenseServerApplication.java
@@ -0,0 +1,13 @@
+package com.logsense.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsenseServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsenseServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logsense/server/LogsenseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logsense.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsenseServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
