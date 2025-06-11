653297fce6b27c456f7be2906166522324d1f3d0
wwf9w0 <aliemrekaradag1@gmail.com>
2025-06-11 20:47:50 +0300

    added swagger

--- src/main/java/com/logsense/server/LogsenseServerApplication.java
+++ src/main/java/com/logsense/server/LogsenseServerApplication.java
@@ -1,11 +1,14 @@
 package com.logsense.server;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class LogsenseServerApplication {
 
 	public static void main(String[] args) {
